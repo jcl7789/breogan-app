972d8cad6455c01d23e4dcd7c3f819248585d697
Juan Carlos Leto <jcletoar@gmail.com>
2019-02-15 22:49:54 -0300

    Fixes menores de estilo

--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockComponent } from './stock/stock.component';
 import { MatTableModule } from '@angular/material';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    FontAwesomeModule
   ],
   declarations: [StockComponent],
   entryComponents: [StockComponent],
